675bd86f3e093f22287e92f056cc00bf960ec938
Phat <cs.phat.nguyen@gmail.com>
2019-01-21 11:29:36 -0600

    test

--- app/src/main/java/com/rjt/b16couriertrackingsystem/MainActivity.java
+++ app/src/main/java/com/rjt/b16couriertrackingsystem/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.widget.Toast.makeText(this
+                , "Hello", Toast.LENGTH_SHORT).show();
     }
 }
