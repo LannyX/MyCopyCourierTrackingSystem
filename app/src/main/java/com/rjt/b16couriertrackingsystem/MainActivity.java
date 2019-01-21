f9e8d607248200b738c24553d5d57f4599836a4c
Lanny <xulanyixuanr@gmail.com>
2019-01-21 11:33:42 -0600

    first commit

--- app/src/main/java/com/rjt/b16couriertrackingsystem/MainActivity.java
+++ app/src/main/java/com/rjt/b16couriertrackingsystem/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //My branch
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
