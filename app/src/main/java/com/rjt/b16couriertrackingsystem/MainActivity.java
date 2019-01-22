112a5865178e84091f856ee4eb6b43a17dd5bb8b
Phat <cs.phat.nguyen@gmail.com>
2019-01-21 22:54:13 -0600

    login is completed

--- app/src/main/java/com/rjt/b16couriertrackingsystem/MainActivity.java
+++ app/src/main/java/com/rjt/b16couriertrackingsystem/MainActivity.java
@@ -10,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android.widget.Toast.makeText(this
-                , "Hello", Toast.LENGTH_SHORT).show();
     }
 }
